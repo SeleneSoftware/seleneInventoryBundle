bd5bf335c942064e68616bcb2f904083621d41aa
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-04-02 12:33:42 -0400

    moving the product and location entities and new CRUD controllers

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Selene\InventoryBundle\Controller\Admin;
+
+use Selene\InventoryBundle\Entity\Location;
+use Selene\InventoryBundle\Entity\Product;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield Field\TextField::new('name');
+        yield Field\TextField::new('sku');
+        yield Field\TextEditorField::new('description');
+        yield Field\AssociationField::new('location'); // ->setQueryBuilder(
+        //         fn (QueryBuilder $queryBuilder) => $queryBuilder->getEntityManager()->getRepository(Location::class)->findBySomeCriteria();
+        // );
+        yield Field\IntegerField::new('qty');
+        yield Field\MoneyField::new('cost')->setNumDecimals(0)->setCurrency('USD');
+        yield Field\BooleanField::new('public');
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Selene\InventoryBundle\Entity;
+
+use Selene\InventoryBundle\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sku = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 4)]
+    private ?string $cost = null;
+
+    #[ORM\Column]
+    private ?int $qty = null;
+
+    #[ORM\Column]
+    private ?bool $public = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    private ?Location $location = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(string $sku): self
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCost(): ?string
+    {
+        return $this->cost;
+    }
+
+    public function setCost(string $cost): self
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    public function getQty(): ?int
+    {
+        return $this->qty;
+    }
+
+    public function setQty(int $qty): self
+    {
+        $this->qty = $qty;
+
+        return $this;
+    }
+
+    public function isPublic(): ?bool
+    {
+        return $this->public;
+    }
+
+    public function setPublic(bool $public): self
+    {
+        $this->public = $public;
+
+        return $this;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?Location $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+}
